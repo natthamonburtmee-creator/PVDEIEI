3cb17367f8c34b125a3cf7f4261628339903e7e7
natthamonburtmee-creator <natthamonburtmee@gmail.com>
2026-07-22 14:12:24 +0700

    Create 12121.py

---
+++ 12121.py
@@ -0,0 +1,34 @@
+-----------------------------------------
+ Prefabricated Vertical Drain Designer
+-----------------------------------------
+
+Pattern
+
+○ Square
+○ Triangular
+
+Spacing (m)
+
+Width (mm)
+
+Thickness (mm)
+
+Ch (cm²/day)
+
+Time (day)
+
+[ CALCULATE ]
+
+-----------------------------------------
+
+dw
+
+de
+
+Fn
+
+Ur
+
+PASS / FAIL
+
+Consolidation Curve
